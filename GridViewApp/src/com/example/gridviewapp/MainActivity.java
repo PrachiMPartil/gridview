5cd80bb8b3068bce2a821aad81f0520b6f55be38
PrachiMPartil <patilprach99@gmail.com>
2014-12-24 16:03:38 +0530

    Determine the expand the image

--- GridViewApp/src/com/example/gridviewapp/MainActivity.java
+++ GridViewApp/src/com/example/gridviewapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gridviewapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
 public class MainActivity extends Activity {
@@ -11,8 +14,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		GridView gridview= (GridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new ImageAdapter(MainActivity.this));
+		gridview.setAdapter(new ImageAdapter(MainActivity.this));
+		
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position,
+					long id) {
+				// TODO Auto-generated method stub
+				
+				
+			}
+		});
 		
 	}
 
