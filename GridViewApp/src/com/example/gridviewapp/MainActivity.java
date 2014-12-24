e88794ae3d9abd6ad3f4dc3ee6dd9d10b2fcb893
PrachiMPartil <patilprach99@gmail.com>
2014-12-24 16:10:30 +0530

    single activity to mainactivity

--- GridViewApp/src/com/example/gridviewapp/MainActivity.java
+++ GridViewApp/src/com/example/gridviewapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gridviewapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,7 +27,9 @@ public class MainActivity extends Activity {
 					long id) {
 				// TODO Auto-generated method stub
 				
-				
+				Intent i=new Intent(MainActivity.this,SingleViewActivity.class);
+				i.putExtra("id", position);
+				startActivity(i);
 			}
 		});
 		
